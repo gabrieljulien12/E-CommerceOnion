33ae8db0f034e8d90a51d4708a29f1ab198e18ae
gabrieljulien12 <onurkismetli227@gmail.com>
2024-05-28 01:31:46 +0300

    catagory,product,detail ve brand entitiesler olusturuldu

---
+++ Core/OnionDomain/Common/Entities/Detail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionDomain.Common.Entities
+{
+    public class Detail : BaseEntity
+    {
+        public Detail()
+        {
+
+        }
+        public Detail(string title, string description, int catagoryıd)
+        {
+            Title = title;
+            Description = description;
+            CatagoryId = catagoryıd;
+        }
+        public required string Title { get; set; }
+        public required string Description { get; set; } 
+        public  required int CatagoryId { get; set; }
+
+        // relation proporties
+        public virtual Catagory Catagory { get; set; }
+    }
+}
