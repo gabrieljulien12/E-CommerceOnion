fa6ab91a191f7161b14fdd19f16c82a800401291
gabrieljulien12 <onurkismetli227@gmail.com>
2024-05-29 00:54:19 +0300

    write repository yapıldı

--- Core/OnionAplication/Interface/Repositories/IWriteRepository.cs
+++ Core/OnionAplication/Interface/Repositories/IWriteRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnionDomain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace OnionAplication.Interface.Repositories
 {
-    public interface IWriteRepository
+    public interface IWriteRepository<T> where T : class,IentityBase, new()
     {
+        Task AddAsync ( T entity); 
+        Task AddRangeAsync (IList<T> entities);
+        Task<T>UpdateAsync (T entity);
+        Task HardDelete (T entity);
     }
 }
--- Infrastructure/OnionPersistance/Repositories/WriteRepository.cs
+++ Infrastructure/OnionPersistance/Repositories/WriteRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using OnionAplication.Interface.Repositories;
+using OnionDomain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,35 @@ using System.Threading.Tasks;
 
 namespace OnionPersistance.Repositories
 {
-    internal class WriteRepository
+    internal class WriteRepository<T> : IWriteRepository<T> where T : class, IentityBase, new()
     {
+
+        private readonly DbContext _dbcontext;
+        public WriteRepository(DbContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
+        private DbSet<T> Baglantı { get => _dbcontext.Set<T>(); }
+
+        public async Task AddAsync(T entity)
+        {
+            await Baglantı.AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IList<T> entities)
+        {
+            await Baglantı.AddRangeAsync(entities);
+        }
+
+        public async Task HardDelete(T entity)
+        {
+            await Task.Run(() => Baglantı.Remove(entity));
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            await Task.Run(() => Baglantı.Update(entity));
+            return entity;
+        }
     }
 }
