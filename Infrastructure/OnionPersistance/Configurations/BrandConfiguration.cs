03f3a88842d10618472fe2ec829b260740bca3b9
gabrieljulien12 <onurkismetli227@gmail.com>
2024-05-28 03:51:03 +0300

    entitylere configler yapıldı

---
+++ Infrastructure/OnionPersistance/Configurations/BrandConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnionDomain.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionPersistance.Configurations
+{
+    public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(256);
+
+            // Entities Test Verisidir
+            Brand brand1 = new()
+            {
+                Id = 1,
+                Name = "Asus",
+                CreatedDate = DateTime.Now,
+                IsDeleted = false,
+            };
+            Brand brand2 = new()
+            {
+                Id = 2,
+                Name = "Apple",
+                CreatedDate = DateTime.Now,
+                IsDeleted = false,
+
+            };
+            Brand brand3 = new()
+            {
+                Id = 3,
+                Name = "Acer",
+                CreatedDate = DateTime.Now,
+                IsDeleted = false,
+
+            };
+            builder.HasData(brand1, brand2, brand3);
+
+        }
+    }
+}
---
+++ Infrastructure/OnionPersistance/Configurations/DetailConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnionDomain.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionPersistance.Configurations
+{
+    public class DetailConfiguration : IEntityTypeConfiguration<Detail>
+    {
+        public void Configure(EntityTypeBuilder<Detail> builder)
+        {
+            Detail detail1 = new()
+            {
+
+                Id = 1,
+                Title ="EkranKartı",
+                Description ="RTX4090",
+                CatagoryId =1,
+                CreatedDate = DateTime.Now,
+                IsDeleted = false,
+            };
+            Detail detail2 = new()
+            {
+
+                Id = 2,
+                Title = "Ram",
+                Description = "DDR5",
+                CatagoryId = 3,
+                CreatedDate = DateTime.Now,
+                IsDeleted = true,
+            };
+            Detail detail3 = new()
+            {
+
+                Id = 3,
+                Title = "Hammadde",
+                Description = "Yün",
+                CatagoryId = 4,
+                CreatedDate = DateTime.Now,
+                IsDeleted = false,
+            };
+            builder.HasData(detail1,detail2, detail3);  
+        }
+    }
+}
